e3440d0e45f60cfaec3a228f0be7fe4217649d3c
rahmanhabib0005 <rahmanhabib0005@gmail.com>
2024-07-12 07:44:19 +0600

    Custom Main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -2,329 +2,105 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(FlutterApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class FlutterApp extends StatelessWidget {
+  const FlutterApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 230, 14, 14)),
-        useMaterial3: true,
+        title: "FlutterApp",
+        theme: ThemeData(primarySwatch: Colors.yellow, useMaterial3: true),
+        debugShowCheckedModeBanner: false,
+        home: DashBoardScreen());
+  }
+}
+
+class DashBoardScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text("Dashboard"),
+        ),
       ),
-      home: const MyHomePage(
-          title: 'I am Habibur Rahman and it is my first flutter app'),
+      body: Container(
+        color: Colors.blue.shade50,
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-  final String title;
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'My App',
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         colorScheme: ColorScheme.fromSeed(
+//             seedColor: const Color.fromARGB(255, 230, 14, 14)),
+//         useMaterial3: true,
+//       ),
+//       home: const MyHomePage(
+//           title: 'I am Habibur Rahman and it is my first flutter app'),
+//     );
+//   }
+// }
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 10;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
 
-  var arr_names = ['Habib','Sakil','Ramim','Baki','Aminul', 'Nasim'];
+//   final String title;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.lightBlueAccent,
-          // backgroundColor: Theme.of(context).colorScheme.onSurface,
-          title: Center(
-              child: Text(
-            "My Flutter",
-            style: TextStyle(color: Colors.white),
-          )),
-        ),
-        // body: Center(
-        //     child: Container(
-        //   width: 100,
-        //   height: 100,
-        //   color: Colors.blueGrey,
-        //   child: const Text("Hello ...."),
-        // )));
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
 
-        // body: Center(
-        //   child: Container(
-        //       width: 300,
-        //       height: 200,
-        //       color: Colors.blueGrey,
-        //       child: Center(
-        //         child: Text(
-        //           "This is center Container",
-        //           style: TextStyle(color: Colors.white),
-        //         ),
-        //       )),
-        // ),
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _counter = 10;
 
-        // body: Text(
-        //   "It's a text widget.",
-        //   style: TextStyle(
-        //       color: Colors.purple,
-        //       fontSize: 25,
-        //       fontWeight: FontWeight.w700,
-        //       backgroundColor: Colors.blueAccent),
-        // ),
-        // body: TextButton(
-        //   child: Text('Click Here!!'),
-        //   onPressed: () {
-        //     print('Text Button Clicked!');
-        //   },
-        //   onLongPress: () {
-        //     print('Long Press Clicked');
-        //   },
-        // ),
-        // body: ElevatedButton(
-        //   child: Text('Presse here !!'),
-        //   onPressed: () {
-        //     print('Working..');
-        //   },
-        // ),
-        // body: OutlinedButton(
-        //   child: Text('oulined'),
-        //   onPressed: () {
-        //     print('work');
-        //   },
-        // ),
-        // body: Center(
-        //   child: Container(
-        //     child: Image.asset('assets/images/flutter.webp'),
-        //   ),
-        // ),
-        // body: Container(
-        //   // width: 320,
-        //   height: 320,
-        //   child: Column(
-        //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        //     crossAxisAlignment: CrossAxisAlignment.start,
-        //     children: [
-        //       Row(
-        //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        //         children: [
-        //           Column(
-        //             children: [
-        //               ElevatedButton(onPressed: () {}, child: Text('Button 1')),
-        //               ElevatedButton(onPressed: () {}, child: Text('Button 2')),
-        //             ],
-        //           ),
-        //           Text(
-        //             "A",
-        //             style: TextStyle(fontSize: 25),
-        //           ),
-        //           Text(
-        //             "B",
-        //             style: TextStyle(fontSize: 25),
-        //           ),
-        //           Text(
-        //             "C",
-        //             style: TextStyle(fontSize: 25),
-        //           ),
-        //           Text(
-        //             "D",
-        //             style: TextStyle(fontSize: 25),
-        //           ),
-        //           Text(
-        //             "E",
-        //             style: TextStyle(fontSize: 25),
-        //           ),
-        //           ElevatedButton(
-        //               onPressed: () {
-        //                 print("Hello");
-        //               },
-        //               child: Text("Click"))
-        //         ],
-        //       ),
-        //       Text(
-        //         "A",
-        //         style: TextStyle(fontSize: 25),
-        //       ),
-        //       Text(
-        //         "B",
-        //         style: TextStyle(fontSize: 25),
-        //       ),
-        //       Text(
-        //         "C",
-        //         style: TextStyle(fontSize: 25),
-        //       ),
-        //       Text(
-        //         "D",
-        //         style: TextStyle(fontSize: 25),
-        //       ),
-        //       Text(
-        //         "E",
-        //         style: TextStyle(fontSize: 25),
-        //       ),
-        //       ElevatedButton(
-        //           onPressed: () {
-        //             print("Hello");
-        //           },
-        //           child: Text("Click"))
-        //     ],
-        //   ),
-        // ));
-        // body: Center(
-        //     child: InkWell(
-        //   onTap: () {
-        //     print('Tapped on Container');
-        //   },
-        //   onLongPress: () {
-        //     print('Long Press on Container');
-        //   },
-        //   onDoubleTap: () {
-        //     print('Double Tap on Container');
-        //   },
-        //   child: Container(
-        //     width: 200,
-        //     height: 200,
-        //     // color: Colors.amber,
-        //     child: Column(
-        //       children: [
-        //         Center(
-        //             child: InkWell(
-        //                 onTap: () {
-        //                   print('Text Tapped');
-        //                 },
-        //                 child: Center(
-        //                   child: Text(
-        //                     'Click Here',
-        //                     style: TextStyle(
-        //                         fontSize: 21, fontWeight: FontWeight.w600),
-        //                   ),
-        //                 ))),
-        //         Center(
-        //           child: Image.asset('assets/images/flutter.webp'),
-        //         )
-        //       ],
-        //     ),
-        //   ),
-        // )));
-        //   body: Padding(
-        //       padding: const EdgeInsets.all(0.8),
-        //       child: SingleChildScrollView(
-        //         child: Column(
-        //           children: [
-        //             SingleChildScrollView(
-        //               scrollDirection: Axis.horizontal,
-        //               child: Padding(
-        //                 padding: EdgeInsets.all(0.8),
-        //                 child: Row(
-        //                   children: [
-        //                     Container(
-        //                       margin: EdgeInsets.only(right: 11),
-        //                       height: 200,
-        //                       width: 200,
-        //                       color: Colors.lightGreen,
-        //                     ),
-        //                     Container(
-        //                       margin: EdgeInsets.only(right: 11),
-        //                       height: 200,
-        //                       width: 200,
-        //                       color: Colors.blue,
-        //                     ),
-        //                     Container(
-        //                       margin: EdgeInsets.only(right: 11),
-        //                       height: 200,
-        //                       width: 200,
-        //                       color: Colors.grey,
-        //                     ),
-        //                     Container(
-        //                       margin: EdgeInsets.only(right: 11),
-        //                       height: 200,
-        //                       width: 200,
-        //                       color: Colors.lightGreen,
-        //                     ),
-        //                     Container(
-        //                       margin: EdgeInsets.only(right: 11),
-        //                       height: 200,
-        //                       width: 200,
-        //                       color: Colors.amberAccent,
-        //                     ),
-        //                     Container(
-        //                       margin: EdgeInsets.only(right: 11),
-        //                       height: 200,
-        //                       width: 200,
-        //                       color: Colors.black87,
-        //                     ),
-        //                   ],
-        //                 ),
-        //               ),
-        //             ),
-        //             Container(
-        //               margin: EdgeInsets.only(bottom: 11),
-        //               height: 200,
-        //               color: Colors.blue,
-        //             ),
-        //             Container(
-        //               margin: EdgeInsets.only(bottom: 11),
-        //               height: 200,
-        //               color: Colors.grey,
-        //             ),
-        //             Container(
-        //               margin: EdgeInsets.only(bottom: 11),
-        //               height: 200,
-        //               color: Colors.grey,
-        //             ),
-        //             Container(
-        //               margin: EdgeInsets.only(bottom: 11),
-        //               height: 200,
-        //               color: Colors.grey,
-        //             ),
-        //           ],
-        //         ),
-        //       )),
-        // );
+//   void _incrementCounter() {
+//     setState(() {
+//       _counter++;
+//     });
+//   }
 
-        // body: Center(
-        //   child: ListView(
-        //     // scrollDirection: Axis.horizontal,
-        //     reverse: true,
-        //   children: [
-        //     Padding(padding: EdgeInsets.all(8.0),child: Text( 'One', style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),),),
-        //     Padding(padding: EdgeInsets.all(8.0),child: Text( 'Two', style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),),),
-        //     Padding(padding: EdgeInsets.all(8.0),child: Text( 'Three', style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),),),
-        //     Padding(padding: EdgeInsets.all(8.0),child: Text( 'Four', style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),),),
-        //     Padding(padding: EdgeInsets.all(8.0),child: Text( 'Five', style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),),),
-        //     Padding(padding: EdgeInsets.all(8.0),child: Text( 'Six', style: TextStyle(fontSize: 21, fontWeight: FontWeight.w500),),),
-        //   ],
-        // )),
-        // );
+//   var arr_names = ['Habib','Sakil','Ramim','Baki','Aminul', 'Nasim'];
 
-        
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: Colors.lightBlueAccent,
+//           // backgroundColor: Theme.of(context).colorScheme.onSurface,
+//           title: Center(
+//               child: Text(
+//             "My Flutter",
+//             style: TextStyle(color: Colors.white),
+//           )),
+//         ),
 
-         body: Center(child: ListView.separated(itemBuilder: (context,index){
-          return Row(
-            children: [
-              Padding(padding: EdgeInsets.all(8.0),child: Text(arr_names[index],style: TextStyle(fontSize: 21,fontWeight: FontWeight.w500),),),
-              Padding(padding: EdgeInsets.all(0.8),child: Text(arr_names[index],style: TextStyle(fontSize: 21,fontWeight: FontWeight.w500),),),
-              Padding(padding: EdgeInsets.all(0.8),child: Text(arr_names[index],style: TextStyle(fontSize: 21,fontWeight: FontWeight.w500),),),
-            ],
-          );
-         },itemCount: arr_names.length,
-         separatorBuilder: (context, index){
-          return Divider(height: 100,thickness: 2,);
-         },
-        //  scrollDirection: Axis.horizontal,
-         ),));
-  }
-}
+//          body: Center(child: ListView.separated(itemBuilder: (context,index){
+//           return Row(
+//             children: [
+//               Padding(padding: EdgeInsets.all(8.0),child: Text(arr_names[index],style: TextStyle(fontSize: 21,fontWeight: FontWeight.w500),),),
+//               Padding(padding: EdgeInsets.all(0.8),child: Text(arr_names[index],style: TextStyle(fontSize: 21,fontWeight: FontWeight.w500),),),
+//               Padding(padding: EdgeInsets.all(0.8),child: Text(arr_names[index],style: TextStyle(fontSize: 21,fontWeight: FontWeight.w500),),),
+//             ],
+//           );
+//          },itemCount: arr_names.length,
+//          separatorBuilder: (context, index){
+//           return Divider(height: 100,thickness: 2,);
+//          },
+//         //  scrollDirection: Axis.horizontal,
+//          ),));
+//   }
+// }
